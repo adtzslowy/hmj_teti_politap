e9632aea5f8041fa5d147bf5b3ec45f855a9eaf7
Yogi_Aza <andriansahyogi12@gmail.com>
2025-11-05 17:21:10 +0700

    final
    fiks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>HMJ TETI POLITAP | Himpunan Mahasiswa Jurusan Teknik Elektro & Teknik Informatika</title>
-  <link rel="icon" href="{{ asset('logo.png') }}">
+  <link rel="icon" href="{{ asset('source/assets/images/logos/favicon.png') }}">
 
   <!-- Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
